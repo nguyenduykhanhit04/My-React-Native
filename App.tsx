31a090a0feed4ab0ae29a9c9177b8aa133d2bb2b
khanh-newbie <nguyenduykhanh121204@gmail.com>
2026-04-02 00:32:31 +0700

    Sau khi học xong cách khởi tạo dự án, các thuộc tính của View, Text, ... Parent và Child

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // jsx: javascript xml
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.container}> 
+      <Text style={styles.text}>Hello World 1</Text> 
+      <Text style={{ color: "pink", fontSize: 40, fontWeight: 'bold'}}>Hello World 2</Text> 
+      
+      <View style={styles.box}>
+        <Text>Hello World 3</Text>
+      </View>
+    
+      <View>
+        <Text style={styles.parentText}>Hello World 4
+          <Text>Hello World 4.1</Text>
+          <Text style={styles.childText}>Hello World 5</Text>
+        </Text>
+        
+      </View>
+    </View> 
   );
 }
 
+// Không có khái niệm CSS trong React Native, chỉ có StyleSheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  box: {
+    width: 120,
+    height: 120,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    margin: 10,
+    padding: 10,
+  },
+  parentText: {
+    color: 'red',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  childText: {
+    color: 'blue',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 "# My-React-Native" 
+- Cài đặt Android Studio và bật máy ảo Android Studio để chạy project
+- Cài đặt dự án React Native với command: npx create-expo-app@latest
+- Chạy dự án với command: expo start
+
+- Nhấn a để chạy dự án trên máy ảo Android
+- Nhấn i để chạy dự án trên máy ảo iOS
+
+- Lưu ý không phải CSS mà là StyleSheet
+- Parent and Child:
+  - Parent: là View cha
+  - Child: là View con
+  - Nếu muốn thuộc tính bên trong Parent không bị Parent ảnh hưởng thì phải đặt thuộc tính style cho Child
+  - Ví dụ:
+    <View style={styles.parent}>
+    <View style={styles.child}>
+        <Text>Hello World 4.1</Text>
+    </View>
+    </View>
+    - Thì thuộc tính style cho Child phải đặt trong styles.child
+    - Thì thuộc tính style cho Parent phải đặt trong styles.parent
